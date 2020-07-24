13d2f539c0da40c21b3e351366e43c39a8e4bd55
JulianaAmoasei <late.parrot@gmail.com>
2020-07-24 17:09:16 -0300

    video 4.2

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -194,11 +194,13 @@ class PessoaController {
   static async cancelaPessoa(req, res) {
     const { estudanteId } = req.params
     try {
-      await database.Pessoas
-        .update({ ativo: false }, { where: { id: Number(estudanteId) } })
-      await database.Matriculas
-        .update({ status: 'cancelado' }, { where: { estudante_id: Number(estudanteId) } })
-      return res.status(200).json({ message: `matrículas ref. estudante ${estudanteId} canceladas` })
+      database.sequelize.transaction(async transacao => {
+        await database.Pessoas
+          .update({ ativo: false }, { where: { id: Number(estudanteId) } }, { transaction: transacao })
+        await database.Matriculas
+          .update({ status: 'cancelado' }, { where: { estudante_id: Number(estudanteId) } }, { transaction: transacao })
+        return res.status(200).json({ message: `matrículas ref. estudante ${estudanteId} canceladas` }) 
+      })
     } catch (error) {
       return res.status(500).json(error.message)
     }
